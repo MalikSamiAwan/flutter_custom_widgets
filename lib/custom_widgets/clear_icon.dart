1043c1ed6aba16c4f4fb3320bed0a0451c708039
samiawan <pakistanionetofive@gmail.com>
2022-02-23 21:35:17 +0500

    clear icon with Icon data type added aded

--- lib/custom_widgets/clear_icon.dart
+++ lib/custom_widgets/clear_icon.dart
@@ -1,5 +1,8 @@
 part of custom_widgets;
 
+Icon clearIcon(){
+  return Icon(Icons.clear,color: Colors.red,size: 8,);
+}
 class ClearIcon extends StatelessWidget {
   const ClearIcon({Key? key}) : super(key: key);
 
