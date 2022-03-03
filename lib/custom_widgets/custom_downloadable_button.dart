1ab49ab57fc6c2a90f1db9f6d7125102999f20ee
samiawan <pakistanionetofive@gmail.com>
2022-03-03 14:23:49 +0500

    download button redesign

--- lib/custom_widgets/custom_downloadable_button.dart
+++ lib/custom_widgets/custom_downloadable_button.dart
@@ -1,38 +1,72 @@
 part of custom_widgets;
 
-
-
 class CustomDownloadableButton extends StatefulWidget {
   Key? customKey;
   String? title;
-  CButtonType? buttonType;
   Future Function()? onPressed;
-   CustomDownloadableButton({Key? key,this.customKey,this.title,this.buttonType,this.onPressed}) : super(key: key);
+  Color? color;
+  CustomDownloadableButton(
+      {Key? key, this.customKey, this.title, this.onPressed, this.color})
+      : super(key: key);
 
   @override
-  _CustomDownloadableButtonState createState() => _CustomDownloadableButtonState();
+  _CustomDownloadableButtonState createState() =>
+      _CustomDownloadableButtonState();
 }
 
 class _CustomDownloadableButtonState extends State<CustomDownloadableButton> {
-  bool isPressed=false;
+  bool isPressed = false;
   @override
   Widget build(BuildContext context) {
-    if(isPressed){
+    if (isPressed) {
       return CustomProgressIndicator();
     }
-    return CNewButton(
-      key: widget.customKey,
-      title: widget.title??'',
-      type: widget.buttonType??CButtonType.home,
-      onClick: ()async{
-        setState(() {
-          isPressed=true;
-        });
-        await widget.onPressed!();
-        setState(() {
-          isPressed=false;
-        });
-      },
+    return CustomOnHoverMouseRegion(
+      customWidget: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+        child: Material(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5),
+          ),
+          color: widget.color ?? AppStyle.customB,
+          child: InkWell(
+            borderRadius: BorderRadius.circular(5),
+            onTap: () async {
+              setState(() {
+                isPressed = true;
+              });
+              await widget.onPressed!();
+              setState(() {
+                isPressed = false;
+              });
+            },
+            child: Padding(
+                padding: AppStyle.buttonPadding,
+                child: Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(text: '${widget.title} ',style: TextStyle(color: Colors.white)),
+                      WidgetSpan(child: Icon(Icons.cloud_download_outlined,color: Colors.white,)),
+                    ],
+                  ),
+                )),
+          ),
+        ),
+      ),
     );
+    //   CNewButton(
+    //   key: widget.customKey,
+    //   title: widget.title??'',
+    //   type: widget.buttonType??CButtonType.home,
+    //   onClick: ()async{
+    //     setState(() {
+    //       isPressed=true;
+    //     });
+    //     await widget.onPressed!();
+    //     setState(() {
+    //       isPressed=false;
+    //     });
+    //   },
+    // );
   }
 }
