ba093f277d058a441123b8d72f08615fac91a9ed
samiawan <pakistanionetofive@gmail.com>
2022-03-28 14:55:34 +0500

    hover effect removed from download button

--- lib/custom_widgets/custom_downloadable_button.dart
+++ lib/custom_widgets/custom_downloadable_button.dart
@@ -5,8 +5,14 @@ class CustomDownloadableButton extends StatefulWidget {
   String? title;
   Future Function()? onPressed;
   Color? color;
+  final bool hoverEffect;
   CustomDownloadableButton(
-      {Key? key, this.customKey, this.title, this.onPressed, this.color})
+      {Key? key,
+      this.customKey,
+      this.title,
+      this.onPressed,
+      this.color,
+      this.hoverEffect = false})
       : super(key: key);
 
   @override
@@ -18,79 +24,121 @@ class _CustomDownloadableButtonState extends State<CustomDownloadableButton> {
   bool isPressed = false;
   @override
   Widget build(BuildContext context) {
-    // if (isPressed) {
-    //   return CustomProgressIndicator();
-    // }
-    return CustomOnHoverMouseRegion(
-      customWidget: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-        child: Material(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(5),
+    if (widget.hoverEffect) {
+      return CustomOnHoverMouseRegion(
+        customWidget: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+          child: Material(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            color: widget.color ?? CupertinoColors.lightBackgroundGray,
+            child: InkWell(
+              borderRadius: BorderRadius.circular(5),
+              onTap: () async {
+                setState(() {
+                  isPressed = true;
+                });
+                await widget.onPressed!();
+                setState(() {
+                  isPressed = false;
+                });
+              },
+              child: Padding(
+                  padding: AppStyle.buttonPadding,
+                  child: Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                            text: '${widget.title}',
+                            style: TextStyle(
+                                color: AppStyle.customB,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12)),
+                        WidgetSpan(
+                            alignment: PlaceholderAlignment.middle,
+                            child: SizedBox(
+                              width: 5,
+                            )),
+                        if (isPressed) ...[
+                          WidgetSpan(
+                              alignment: PlaceholderAlignment.middle,
+                              child: CupertinoActivityIndicator(
+                                radius: 10,
+                                color: CupertinoColors.activeBlue,
+                              )),
+                        ] else ...[
+                          WidgetSpan(
+                              alignment: PlaceholderAlignment.middle,
+                              child: Icon(
+                                Icons.cloud_download_outlined,
+                                color: CupertinoColors.activeBlue,
+                                size: 12,
+                              )),
+                        ]
+                      ],
+                    ),
+                  )),
+            ),
           ),
-          color: widget.color ?? CupertinoColors.lightBackgroundGray,
-          child: InkWell(
-            borderRadius: BorderRadius.circular(5),
-            onTap: () async {
-              setState(() {
-                isPressed = true;
-              });
-              await widget.onPressed!();
-              setState(() {
-                isPressed = false;
-              });
-            },
-            child: Padding(
-                padding: AppStyle.buttonPadding,
-                child: Text.rich(
-                  TextSpan(
-                    children: [
-                      TextSpan(
-                          text: '${widget.title}',
-                          style: TextStyle(
+        ),
+      );
+    }
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      child: Material(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
+        ),
+        color: widget.color ?? CupertinoColors.lightBackgroundGray,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(5),
+          onTap: () async {
+            setState(() {
+              isPressed = true;
+            });
+            await widget.onPressed!();
+            setState(() {
+              isPressed = false;
+            });
+          },
+          child: Padding(
+              padding: AppStyle.buttonPadding,
+              child: Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                        text: '${widget.title}',
+                        style: TextStyle(
                             color: AppStyle.customB,
-                              fontWeight: FontWeight.bold,
-                            fontSize: 12
-                          )),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12)),
+                    WidgetSpan(
+                        alignment: PlaceholderAlignment.middle,
+                        child: SizedBox(
+                          width: 5,
+                        )),
+                    if (isPressed) ...[
                       WidgetSpan(
                           alignment: PlaceholderAlignment.middle,
-                          child: SizedBox(
-                            width: 5,
+                          child: CupertinoActivityIndicator(
+                            radius: 10,
+                            color: CupertinoColors.activeBlue,
                           )),
-                      if (isPressed) ...[
-                        WidgetSpan(
-                            alignment: PlaceholderAlignment.middle,
-                            child: CupertinoActivityIndicator(
-                              radius: 10,color: CupertinoColors.activeBlue,)),
-                      ] else ...[
-                        WidgetSpan(
-                            alignment: PlaceholderAlignment.middle,
-                            child: Icon(
-                              Icons.cloud_download_outlined,
-                              color: CupertinoColors.activeBlue,
-                              size: 12,
-                            )),
-                      ]
-                    ],
-                  ),
-                )),
-          ),
+                    ] else ...[
+                      WidgetSpan(
+                          alignment: PlaceholderAlignment.middle,
+                          child: Icon(
+                            Icons.cloud_download_outlined,
+                            color: CupertinoColors.activeBlue,
+                            size: 12,
+                          )),
+                    ]
+                  ],
+                ),
+              )),
         ),
       ),
     );
-    //   CNewButton(
-    //   key: widget.customKey,
-    //   title: widget.title??'',
-    //   type: widget.buttonType??CButtonType.home,
-    //   onClick: ()async{
-    //     setState(() {
-    //       isPressed=true;
-    //     });
-    //     await widget.onPressed!();
-    //     setState(() {
-    //       isPressed=false;
-    //     });
-    //   },
-    // );
   }
 }
