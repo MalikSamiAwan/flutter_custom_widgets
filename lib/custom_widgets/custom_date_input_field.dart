af839e42e1fbccdbdce17a7c4af7b4b7931c45cb
samiawan <pakistanionetofive@gmail.com>
2022-05-18 22:12:28 +0500

    date formating

--- lib/custom_widgets/custom_date_input_field.dart
+++ lib/custom_widgets/custom_date_input_field.dart
@@ -12,11 +12,13 @@ class CDateField extends StatelessWidget {
   final double order;
   Key? testingKey;
   final DateTime? firstDate;
+  final bool justSameFormattingRequired;
 
   CDateField({Key? key,testingKey, required this.title, this.onChanged, this.value,this.validator,
     this.autoFocus=false,
     this.order=0,
-    this.firstDate
+    this.firstDate,
+    this.justSameFormattingRequired=false,
     })
       : super(key: key);
 
@@ -25,6 +27,49 @@ class CDateField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     controller.text = value.formattedData;
+    if(justSameFormattingRequired){
+      return Container(
+        padding: AppStyle.fieldPaddingGlobal,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: AppStyle.fieldTitlePadding,
+              child: Text(
+                title,
+                style: AppStyle.fieldTitleTextStyle,
+              ),
+            ),
+            FocusTraversalOrder(
+              order: NumericFocusOrder(order),
+              child: TextFormField(
+                controller: controller,
+                readOnly: true,
+                textAlignVertical: TextAlignVertical.center,
+                decoration: InputDecoration(
+
+                  border: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  errorBorder: InputBorder.none,
+                  disabledBorder: InputBorder.none,
+                  errorStyle: AppStyle.errorTextStyle,
+                  counterStyle: AppStyle.counterTextStyle,
+                  counterText: 'Counter Text',
+                  contentPadding: EdgeInsets.zero,
+                  prefixIcon: Icon(
+                    Icons.calendar_today_outlined,
+                    color: Colors.transparent,
+                    size: 15,
+                  ),
+                  isDense: true,
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+    }
     return Container(
       padding: AppStyle.fieldPaddingGlobal,
       child: Column(
